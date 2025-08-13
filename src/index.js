7f3d2776958da1a31f3bbe353c0d28ef93eeb7ac
joaogabriel1702 <joaogabrielcsoares@hotmail.com>
2025-08-13 14:21:02 -0300

    Chamada do prompt-main para testes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import prompt from "prompt"
+import mainPrompt from "./prompt/prompt-main.js"
 
 async function main() {
-    
+    prompt.get(mainPrompt, async (err, choose) => {
+        if(choose.select == 1) console.log("Escolheu o qrCode")
+        if(choose.select == 2) console.log("Escolheu o password")
+    })
 }
 
 main()
